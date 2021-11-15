ab87072455914c5493464d1d133334dfdccdccdd
Athcns <austinkim325@gmail.com>
2021-11-15 08:50:24 -0800

    TODO: Connect the Journal Model to the Entry Model

--- akeso/journal/models.py
+++ akeso/journal/models.py
@@ -1,21 +1,23 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Journal(models.Model):
     # Assigns each account user's primary key to a journal table
-    writer = models.ForeignKey( PLACEHOLDER )
+    writer = models.ManyToManyField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.id}"
 
 class Entry(models.Model):
     # Accesses the entries with the same journal primary keys
-    journal_id = models.ForeignKey(Journal, on_delete=models.CASCADE, related_name="book")
+    # TODO: Add a connection between the journal_id and each user's journals
+    #journal_id = models.ManyToManyField(Journal, on_delete=models.CASCADE, related_name="book")
     # Creation date is set using datetime.datetime in views.py
-    creation_date = models.DateTimeField
+    creation_date = models.DateTimeField(auto_now_add=True)
     # Auto sets date for when the model is opened
     accessed_date = models.DateTimeField(auto_now=True)
     # Header for the journal | max 100 chars
     header = models.CharField(max_length=100)
     # Entry Details
-    details = models.CharField
+    content = models.TextField()
--- akeso/journal/views.py
+++ akeso/journal/views.py
@@ -3,13 +3,17 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate
 from django.urls import reverse
 
+from .models import Journal, Entry
+
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
     else:
-        return render("journal/journal.html", {
-            "user_id": request.user.id
+        library = Journal.objects.all()
+        return render(request, "journal/journal.html", {
+            "user_id": request.user.id,
+            "journals": library
         })
 
 def create(request):
@@ -18,4 +22,18 @@ def create(request):
     else:
         if request.method == "POST":
             header = request.POST['header']
-            text = request.POST['text']
+            content = request.POST['content']
+
+            # Creates a new entry model and saves it
+            new_entry = Entry(header=header, content=content)
+            new_entry.save()
+
+            return render(request, "journal/journal.html", {
+                "user_id": request.user.id
+            })
+        library = Journal.objects.all()
+        return render(request, "journal/create.html", {
+        })
+
+def read(request):
+    pass
