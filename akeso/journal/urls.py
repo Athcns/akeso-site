7fffa886b6a730a215cc5b0b6ff4952c53f737ed
Athcns <austinkim325@gmail.com>
2021-11-15 21:58:11 -0800

    Changed few urls and added one new one

--- akeso/journal/urls.py
+++ akeso/journal/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create", views.create, name="create"),
+    path("<int:journalID>/create", views.create_entry, name="createEntry"),
     path("read", views.read, name="read"),
-    path("create-journal", views.create_journal, name="createJournal")
+    path("create-journal", views.create_journal, name="createJournal"),
+    path("<int:journalID>", views.journal_view, name="viewJournal")
 ]
