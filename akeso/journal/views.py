dac621de08a86f1d92c16c905ab7879c4d99906c
Athcns <austinkim325@gmail.com>
2021-11-15 22:04:00 -0800

    Changed "create" to "create_entry" and created a new function called "create_journal" + "journal_view". Got the basic feature of creating journals, viewing them in a library, and creating entries for each journal created.

--- akeso/journal/views.py
+++ akeso/journal/views.py
@@ -6,6 +6,8 @@ from django.urls import reverse
 from .models import Journal, Entry
 from django.contrib.auth.models import User
 
+#TODO: Create a way to view the entries details (Their Context and Header)
+
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
@@ -14,40 +16,67 @@ def index(request):
         user = User.objects.get(id=request.user.id)
         library = Journal.objects.filter(writer=user)
 
-        return render(request, "journal/journal.html", {
-            "user_id": request.user.id,
+        # TODO: Allow users to see their Journals numbered 1, 2, 3,... rather than just the
+        # primary key number auto assigned to the journal on creation.
+        return render(request, "journal/library.html", {
             "journals": library
         })
 
-def create(request):
+
+def create_entry(request, journalID):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
     else:
+        journal = Journal.objects.get(id=journalID)
+
         if request.method == "POST":
             header = request.POST['header']
             content = request.POST['content']
 
             # Creates a new entry model and saves it
-            new_entry = Entry(header=header, content=content)
-            new_entry.save()
+            newEntry = Entry(header=header, content=content)
+            newEntry.save()
+            newEntry.journal_id.add(journal)
 
-            return render(request, "journal/journal.html")
-        library = Journal.objects.all()
+            entries = Entry.objects.filter(journal_id=journalID)
+            return render(request, "journal/journal.html", {
+                "journal":journal,
+                "entries":entries
+            })
+
         return render(request, "journal/create.html", {
+            "journal": journal
         })
+
 
 def create_journal(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
     else:
         if request.method == "POST":
-            name = request.POST['journal-name']
+            name = request.POST['journalName']
             user = User.objects.get(id=request.user.id)
 
-            new_journal =  Journal(writer=user, name=name)
-            new_journal.save()
+            newJournal = Journal(writer=user, name=name)
+            newJournal.save()
 
             return HttpResponseRedirect(reverse("index"))
         return HttpResponseRedirect(reverse("index"))
+
+
+def journal_view(request, journalID):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    else:
+        journal = Journal.objects.get(id=journalID)
+        entries = Entry.objects.filter(journal_id=journalID)
+
+        return render(request, "journal/journal.html", {
+            "journal": journal,
+            "entries": entries
+        })
+
+
+
 def read(request, entry_id):
     pass
