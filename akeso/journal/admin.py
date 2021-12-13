c325bd9188c0f05a4b6ef33e93eb4700373c2b46
Athcns <austinkim325@gmail.com>
2021-12-12 21:47:43 -0800

    Added Mood related models to admin panel

--- akeso/journal/admin.py
+++ akeso/journal/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from .models import Journal, Entry, Activity, Mood, Status, WeeklyUpdate
 
 # Register your models here.
+# Journal related
 admin.site.register(Journal)
 admin.site.register(Entry)
+
+# Mood related
 admin.site.register(Activity)
 admin.site.register(Mood)
 admin.site.register(Status)
