b693bdc6ad7c58b3c5e3c2265cec27ddb3cfd677
Athcns <austinkim325@gmail.com>
2021-12-12 21:46:03 -0800

    Fixed creating an activity

--- akeso/journal/admin.py
+++ akeso/journal/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Journal, Entry
+from .models import Journal, Entry, Activity, Mood, Status, WeeklyUpdate
 
 # Register your models here.
 admin.site.register(Journal)
 admin.site.register(Entry)
+admin.site.register(Activity)
+admin.site.register(Mood)
+admin.site.register(Status)
+admin.site.register(WeeklyUpdate)
