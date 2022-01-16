012be5e2a4979664da42415d5f3658261da714de
Athcns <austinkim325@gmail.com>
2022-01-15 22:53:40 -0800

    added urls for new options

--- akeso/journal/urls.py
+++ akeso/journal/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("settings", views.settings, name="settings"),
     path("create-mood", views.create_mood, name="mood"),
     path("delete-mood", views.delete_mood, name="deleteMood"),
     path("activities", views.view_activity, name="viewActivity"),
@@ -14,6 +15,8 @@ urlpatterns = [
     path("delete-weekly-report", views.delete_weekly_update, name="deleteWeeklyReport"),
     path("<int:journalID>/create", views.create_entry, name="createEntry"),
     path("<int:journalID>/entry/<int:entryID>", views.view_entry, name="readEntry"),
+    path("<int:journalID>/entry-delete/<int:entryID>", views.delete_entry, name="deleteEntry"),
     path("create-journal", views.create_journal, name="createJournal"),
-    path("<int:journalID>", views.view_journal, name="viewJournal")
+    path("<int:journalID>", views.view_journal, name="viewJournal"),
+    path("delete-journal/<int:journalID>", views.delete_journal, name="deleteJournal")
 ]
