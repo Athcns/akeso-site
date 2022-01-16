72a0ce96ea7345c01300b977527c444950df6893
Athcns <austinkim325@gmail.com>
2022-01-15 22:45:11 -0800

    Added a way to change passwords and change first name

--- akeso/users/urls.py
+++ akeso/users/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="homepage"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("change-password", views.change_password, name="changeCurrentPassword"),
+    path("change-name", views.change_name, name="changeName")
 ]
